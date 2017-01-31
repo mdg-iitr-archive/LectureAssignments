6e4e9d23a1714dd19e63ae39c43664a46ff0d02e
arihant <aridj456@gmail.com>
2017-01-31 23:31:44 +0530

    Adding Button in MainActivity

--- app/src/main/java/com/sdsmdg/hareshkh/lectureassignment/MainActivity.java
+++ app/src/main/java/com/sdsmdg/hareshkh/lectureassignment/MainActivity.java
@@ -5,15 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.sdsmdg.hareshkh.lectureassignment.arihant.Activities.RecyclerViewActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button rvBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, RecyclerViewActivity.class));
+        rvBtn = (Button)findViewById(R.id.recycler_view_btn);
+        rvBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RecyclerViewActivity.class));
+            }
+        });
     }
 }

549738783f4e5416d075648077aef5f3b7f36bba
dakshitagrawal97 <neelagrawal08@gmail.com>
2017-02-05 23:41:07 +0530

    added FAB listener to main activity

--- app/src/main/java/com/sdsmdg/hareshkh/lectureassignment/MainActivity.java
+++ app/src/main/java/com/sdsmdg/hareshkh/lectureassignment/MainActivity.java
@@ -1,9 +1,12 @@
 package com.sdsmdg.hareshkh.lectureassignment;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +41,15 @@ public class MainActivity extends AppCompatActivity {
         listOfMoviesRecyclerView.setAdapter(adapter);
         listOfMoviesRecyclerView.setHasFixedSize(true);
         listOfMoviesRecyclerView.setLayoutManager(linearLayout);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButtonToCustomActivity);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 

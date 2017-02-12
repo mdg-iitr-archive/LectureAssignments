c69e7ec99638f5892db428e74141cdf790cf3684
dakshitagrawal97 <neelagrawal08@gmail.com>
2017-02-12 11:11:34 +0530

    Dakshit's Assignment for Harjot's both lectures.
    * changed the build.gradle file
    
    * included recyclerview dependency
    
    * MainActivity.java file containing recyclerview
    
    * activity_main.xml file containing recyclerview
    
    * changed the appbar title
    
    * contains all posters used in app
    
    * created a model for movies
    
    * created a layout for recyclerview item
    
    * code for the item viewholder
    
    * wrote code for recycler view adapter
    
    * added design dependency
    
    * added second activity
    
    * added FAB listener to main activity
    
    * added FAB
    
    * changed accent color
    
    * added new activity for custom views
    
    * added custom view code
    
    * added right_arrow drawable
    
    * added custom behaviour of snackbar to seekbar
    
    * layout of custom view activity
    
    * attributes file for the progress rectangle custom view
    
    * added custom behavior to progress rectangle view
    
    * updated layout for custom view activity

---
+++ app/src/main/java/com/sdsmdg/hareshkh/lectureassignment/Adapter.java
@@ -0,0 +1,93 @@
+package com.sdsmdg.hareshkh.lectureassignment;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by shyam on 31-Jan-17.
+ */
+
+public class Adapter extends RecyclerView.Adapter<ItemViewHolder> {
+    int moviesCount;
+    Context context;
+    ItemViewHolder itemViewHolder;
+    List<MoviesModel> moviesList;
+
+    public Adapter(Context context, List<MoviesModel> moviesList){
+        this.context = context;
+        moviesCount = moviesList.size();
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item,parent,false);
+        itemViewHolder = new ItemViewHolder(view);
+        return itemViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, final int position) {
+        holder.movieName.setText(moviesList.get(position).getMovieName());
+        holder.movieReleaseDate.setText(moviesList.get(position).getMovieReleaseDate());
+        holder.moviePoster.setImageBitmap(decodeSampledBitmapFromResource(context.getResources(),moviesList.get(position).getMoviePoster(),55,90));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"You clicked on Movie Number " + (position+1),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesCount;
+    }
+
+    public static int calculateInSampleSize( BitmapFactory.Options options, int reqWidth, int reqHeight) {
+
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+}
